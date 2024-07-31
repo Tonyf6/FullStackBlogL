a4b015251e3f0e1f875555ecd1a50522a5d7614a
Tonyf6 <149448421+Tonyf6@users.noreply.github.com>
2024-07-30 19:45:23 -0700

    Day 2

--- api/Models/UserModel.cs
+++ api/Models/UserModel.cs
@@ -7,6 +7,13 @@ namespace api.Models
 {
     public class UserModel
     {
-        
+        public int Id {get; set; }
+        public string? Username {get; set; }
+        public string? Salt {get; set; }
+        public string? Hash {get; set; }
+        public UserModel()
+        {
+
+        }
     }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a1cea501b9db80d605ae3351874c20bdf03257")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb28afe933ce8bafb14d785ab13bdfbab74b94c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
